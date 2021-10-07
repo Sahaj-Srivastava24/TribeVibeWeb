8e3d118b8bf33954bbe3521a1e34442d5f66bb2c
LkshyChhabra <72693509+LkshyChhabra@users.noreply.github.com>
2021-10-08 00:10:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 // import Layout from './FoodOrder/Layout'
-import Layout from './TODO-List/Layout';
+// import Layout from './TODO-List/Layout';
 import TribeVibe from './TribeVibe/HomePage'
-
+// import Timeline from './TribeVibe/Timeline/timeline'
+// import Radium, {StyleRoot} from 'radium';
 
 function App() {
 
   return (
   <TribeVibe/>
+  // <Timeline/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './TribeVibe/Assets/DalekPinpointBold.ttf';
 import registerServiceWorker from './registerServiceWorker';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
