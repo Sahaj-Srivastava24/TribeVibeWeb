288a8ed5d1e16b26e8516dde92763b8ff57da60b
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-08 12:15:16 +0530

    Updated image path

--- src/societies/data.js
+++ src/societies/data.js
@@ -1,22 +1,22 @@
 const society = [{
     "name" : "backslash",
-    "img1" : "Screenshot.png",
-    "img2" : "Screenshot.png",
-    "img3" : "Screenshot.png",
+    "img1" : "./TribeVibe/Assets/Screenshot.png",
+    "img2" : "./TribeVibe/Assets/Screenshot.png",
+    "img3" : "./TribeVibe/Assets/Screenshot.png",
     "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
 },
 {
     "name" : "backslash",
-    "img1" : "Screenshot.png",
-    "img2" : "Screenshot.png",
-    "img3" : "Screenshot.png",
+    "img1" : "./TribeVibe/Assets/Screenshot.png",
+    "img2" : "./TribeVibe/Assets/Screenshot.png",
+    "img3" : "./TribeVibe/Assets/Screenshot.png",
     "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
 },
 {
     "name" : "backslash",
-    "img1" : "Screenshot.png",
-    "img2" : "Screenshot.png",
-    "img3" : "Screenshot.png",
+    "img1" : "./TribeVibe/Assets/Screenshot.png",
+    "img2" : "./TribeVibe/Assets/Screenshot.png",
+    "img3" : "./TribeVibe/Assets/Screenshot.png",
     "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
 }]
 
